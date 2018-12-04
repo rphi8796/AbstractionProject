dc32e1d8b3eebc567328edf1eb2ccbe1682efd06
rphi8796 <jlkfop@gmail.com>
2018-12-04 13:19:52 -0700

    Shhhhhh

---
+++ AbstractionProject/Controller/AbstractionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AbstractionProject
+//
+//  Created by Phillips, Ryan on 12/4/18.
+//  Copyright © 2018 Phillips, Ryan. All rights reserved.
+//
+
+import UIKit
+
+public class AbstractionViewController: UIViewController
+{
+
+    public override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
