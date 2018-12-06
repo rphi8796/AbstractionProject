61cdde187b3cb7c8302982b00f62cd5d07780d5f
rphi8796 <jlkfop@gmail.com>
2018-12-06 11:41:40 -0700

    I finished stepped one

--- AbstractionProject/Controller/AbstractionViewController.swift
+++ AbstractionProject/Controller/AbstractionViewController.swift
@@ -1,22 +1,30 @@
 //
-//  ViewController.swift
+//  AbstractionViewController.swift
 //  AbstractionProject
 //
-//  Created by Phillips, Ryan on 12/4/18.
+//  Created by Phillips, Ryan on 12/6/18.
 //  Copyright © 2018 Phillips, Ryan. All rights reserved.
 //
 
 import UIKit
 
-public class AbstractionViewController: UIViewController
-{
+class AbstractionViewController: UIPageViewController {
 
-    public override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
